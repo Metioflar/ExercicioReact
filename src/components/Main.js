4dbd3b9d1715c969bb3493e385928418da1dbf41
Nico <112582889+Metioflar@users.noreply.github.com>
2023-08-07 11:55:13 -0300

    Add files via upload

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,9 +1,16 @@
 import React, {Component} from "react";
 import "./Main.css";
-import {FaBeer} from 'react-icons/fa';
+import {FaPlus} from 'react-icons/fa';
 export default class Main extends Component{
     state = {
         novaTarefa: 'Lista de tarefas',
+        tarefas:[
+            "Tomar banho",
+            "Café da manhã",
+            "Estudar",
+            "almoçar",
+            "Jogar Vavazim"
+        ],
         };
     mudaInput = (e) => {
         this.setState({
@@ -11,14 +18,23 @@ export default class Main extends Component{
         });
     }
     render(){
-        const {novaTarefa} = this.state
+        const {novaTarefa, tarefas} = this.state
         return (
             <div className="main">
                 <h1>{novaTarefa}</h1>
                 <form action="#" className="form">
                     <input onChange={this.mudaInput} type="text"></input>
-                    <button type="submit"><FaBeer/>Cervejinha!</button>
+                    <button type="submit"><FaPlus/></button>
                 </form>
+                <ul className="listaTarefas">
+                    
+                        {tarefas.map(tarefa=>{
+                          return (<li key ={tarefa}>
+                            {tarefa}   
+                          </li> )
+                        })}
+                    
+                </ul>
             </div>
             )
     }
